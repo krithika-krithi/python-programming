8657cac10e0d273d23fd333a857556b6b6a5e809
krithika-krithi <37337159+krithika-krithi@users.noreply.github.com>
2018-04-27 12:14:50 +0530

    Update 9.py

--- beginner level/set-6/9.py
+++ beginner level/set-6/9.py
@@ -1,3 +1,6 @@
-for i in range(1):
-  a=raw_input().split()
-print (max(a))
+a=raw_input().split()
+for i in range(len(a)):
+  for j in range(i+1,len(a)):
+    if (int(a[i])>int(a[j])):
+      a[i],a[j] = a[j],a[i]
+print (a[9])
