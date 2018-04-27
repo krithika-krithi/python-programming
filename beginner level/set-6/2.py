7246c9b605a97f71d5a04a32396ceddcd2f5483b
krithika-krithi <37337159+krithika-krithi@users.noreply.github.com>
2018-04-27 11:04:40 +0530

    Update 2.py

--- beginner level/set-6/2.py
+++ beginner level/set-6/2.py
@@ -1,3 +1,3 @@
-l=['zero','one','two','three','four','five','six','seven','eight','nine','ten']
+l=['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten']
 a=input()
 print l[a]
