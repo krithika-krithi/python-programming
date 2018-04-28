8f0ec659a4529f54ed4d26bf9f7ecf89580211f5
krithika-krithi <37337159+krithika-krithi@users.noreply.github.com>
2018-04-28 11:31:32 +0530

    Update 3.py

--- beginner level/set-8/3.py
+++ beginner level/set-8/3.py
@@ -2,10 +2,10 @@ a=input()
 m,n=raw_input().split()
 m=int(m)
 n=int(n)
-for i in range(m,n):
+for i in range(m+1,n):
   if i==a:
     print ('yes')
     break
-  else:
-    print ('no')
-    break
+else:
+  print ('no')
+    
